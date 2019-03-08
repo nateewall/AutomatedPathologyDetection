d12a5eebe1bf2c69cba62bffbcb3fa26c0137069
nateewall <nateewall@gmail.com>
2019-03-07 23:42:46 -0500

    corrected local cnn

--- code/InitialDataExploration.py
+++ code/InitialDataExploration.py
@@ -4,28 +4,33 @@ from keras import layers
 from keras import models
 from keras import optimizers
 
-train=pd.read_csv("CheXpert-v1.0-small/train.csv", sep=',', dtype = str, na_filter=False)
+#read in the training dataset
+train=pd.read_csv("CheXpert-v1.0-small/train.csv", dtype = str)
 
-# #rename my columns to make it easier to work with
-# df.columns = ['Path', 'Sex', 'FrontalLateral', 'APPA','Age', 'No_Finding','Enlarged_Cardiomediastinum',
-#               'Cardiomegaly',  'Lung_Opacity','Lung_Lesion', 'Edema', 'Consolidation', 'Pneumonia',
-#               'Atelectasis', 'Pneumothorax', 'Pleural_Effusion', 'Pleural_Other', 'Fracture', 'Support_Devices']
+#generate validation set
+valid=pd.read_csv("CheXpert-v1.0-small/valid.csv", dtype = str)
 
 
 #convert the unknown to zero
-train.loc[train['Pleural Effusion'].isna()] = '0.0'
-train.loc[train['Pleural Effusion'] == '-1.0'] = '0.0'
-train.loc[train['Pleural Effusion'] == ''] = '0.0'
-#pull a subset out for testing
-train_subset = train[0:1000].copy()
+train['Pleural Effusion'].loc[train['Pleural Effusion'].isna()] = '0.0'
+train['Pleural Effusion'].loc[train['Pleural Effusion'] == '-1.0'] = '0.0'
 
-# print(train['Path'][0:100])
-# print(train[0:10])
+#pull a subset out for testing
+train_subset = train.sample(10000)
+
+#show what my distribution looks like
 print(train_subset['Pleural Effusion'].value_counts())
-print(train_subset['Path'].value_counts())
 
 #declare the datagen options
-train_datagen = ImageDataGenerator(rescale=1./255)
+train_datagen = ImageDataGenerator(rescale=1./255,
+                                   zca_epsilon=1e-06,
+                                   # randomly shift images horizontally (fraction of total width)
+                                   width_shift_range=0.1,
+                                   # randomly shift images vertically (fraction of total height)
+                                   height_shift_range=0.1,
+                                   horizontal_flip=True,  # randomly flip images
+                                   vertical_flip=False  # randomly flip images
+                                   )
 
 #generate training dataset
 train_generator = train_datagen.flow_from_dataframe(dataframe=train_subset,
@@ -38,64 +43,64 @@ train_generator = train_datagen.flow_from_dataframe(dataframe=train_subset,
                                                     batch_size=20,
                                                     validate_filenames= True)
 
-# #generate validation set
-# valid=pd.read_csv("CheXpert-v1.0-small/valid.csv", dtype = str)
+#convert the unknown to zero
+valid['Pleural Effusion'].loc[valid['Pleural Effusion'].isna()] = '0.0'
+valid['Pleural Effusion'].loc[valid['Pleural Effusion'] == '-1.0'] = '0.0'
 
-# # #rename my columns to make it easier to work with
-# # df.columns = ['Path', 'Sex', 'FrontalLateral', 'APPA','Age', 'No_Finding','Enlarged_Cardiomediastinum',
-# #               'Cardiomegaly',  'Lung_Opacity','Lung_Lesion', 'Edema', 'Consolidation', 'Pneumonia',
-# #               'Atelectasis', 'Pneumothorax', 'Pleural_Effusion', 'Pleural_Other', 'Fracture', 'Support_Devices']
-#
-#
-# #convert the unknown to zero
-# valid.loc[valid['Pleural Effusion'].isna()] = '0.0'
-# valid.loc[valid['Pleural Effusion'] == '-1.0'] = '0.0'
-#
-# print(valid['Pleural Effusion'].value_counts())
-#
-#
-# test_datagen = ImageDataGenerator(rescale=1./255)
-#
-# validation_generator = test_datagen.flow_from_dataframe(dataframe=valid,
-#                                                     directory=None,
-#                                                     x_col="Path",
-#                                                     y_col="Pleural Effusion",
-#                                                     class_mode="binary",
-#                                                     color_mode="grayscale",
-#                                                     target_size=(150,150),
-#                                                     batch_size=20,
-#                                                     validate_filenames= True)
-#
-#
-# model = models.Sequential()
-# model.add(layers.Conv2D(32, (3, 3), activation='relu',
-#                         input_shape=(150, 150, 1)))
-# model.add(layers.MaxPooling2D((2, 2)))
-# model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-# model.add(layers.MaxPooling2D((2, 2)))
-# model.add(layers.Conv2D(128, (3, 3), activation='relu'))
-# model.add(layers.MaxPooling2D((2, 2)))
-# model.add(layers.Conv2D(128, (3, 3), activation='relu'))
-# model.add(layers.MaxPooling2D((2, 2)))
-# model.add(layers.Flatten())
-# model.add(layers.Dense(512, activation='relu'))
-# model.add(layers.Dense(1, activation='sigmoid'))
-#
-#
-#
-# model.compile(loss='binary_crossentropy',
-#               optimizer=optimizers.RMSprop(lr=1e-4),
-#               metrics=['acc'])
-#
-#
-# for data_batch, labels_batch in train_generator:
-#     print('data batch shape:', data_batch.shape)
-#     print('labels batch shape:', labels_batch.shape)
-#     break
-#
-# history = model.fit_generator(
-#       train_generator,
-#       steps_per_epoch=50,
-#       epochs=10,
-#       validation_data=validation_generator,
-#       validation_steps=50)
+print(valid['Pleural Effusion'].value_counts())
+
+test_datagen = ImageDataGenerator(rescale=1./255,
+                                   zca_epsilon=1e-06,
+                                   # randomly shift images horizontally (fraction of total width)
+                                   width_shift_range=0.1,
+                                   # randomly shift images vertically (fraction of total height)
+                                   height_shift_range=0.1,
+                                   horizontal_flip=True,  # randomly flip images
+                                   vertical_flip=False  # randomly flip images
+                                   )
+
+
+validation_generator = test_datagen.flow_from_dataframe(dataframe=valid,
+                                                    directory=None,
+                                                    x_col="Path",
+                                                    y_col="Pleural Effusion",
+                                                    class_mode="binary",
+                                                    color_mode="grayscale",
+                                                    target_size=(150,150),
+                                                    batch_size=20,
+                                                    validate_filenames= True)
+
+
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu',
+                        input_shape=(150, 150, 1)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Flatten())
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+
+
+model.compile(loss='binary_crossentropy',
+              optimizer=optimizers.RMSprop(lr=1e-4),
+              metrics=['acc'])
+
+
+for data_batch, labels_batch in train_generator:
+    print('data batch shape:', data_batch.shape)
+    print('labels batch shape:', labels_batch.shape)
+    break
+
+history = model.fit_generator(
+      train_generator,
+      steps_per_epoch=25,
+      epochs=10,
+      validation_data=validation_generator,
+      validation_steps=25)
